ca39a16fffb29d51a92eb03fec8cac5194064001
DhiralKaniya <dhiralkaniya3146@gmail.com>
2016-10-28 14:10:32 +0530

    Creation of Contact_Directory

---
+++ src/Contact_Dictionary/Solution.java
@@ -0,0 +1,8 @@
+package Contact_Dictionary;
+
+public class Solution {
+	public static void main(String[] args){
+		Contact_Dictionary contact = new Contact_Dictionary();
+		contact.printData();
+	}
+}	
